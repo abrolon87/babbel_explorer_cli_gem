30347927c6de0931915f3de2f802e50d716fcebe
abrolon87 <abrolon87@gmail.com>
2020-02-02 15:32:38 -0500

    refactoring

--- lib/babbel_explorer/cli.rb
+++ lib/babbel_explorer/cli.rb
@@ -4,21 +4,28 @@ class BabbelExplorer::CLI
 
   def call 
     puts "\nWelcome to Babbel Explorer!\n".cyan.bold
-    explore 
+    menu
+  end 
+  
+  def menu 
+    puts "\nTo view country list, type 'explore'\n".green
+    puts "Type 'exit' to exit.".green
+    input = gets.strip
+    case input.downcase 
+    when "explore"
+      explore
+    when "exit"
+      exit
+    else invalid_input 
+    end
   end 
   
   def explore
-    puts "\nScroll down to view country list\n".green
-    #puts "\nTo view country list, type 'explore'\n".green
-    puts "Type 'exit' to exit.".green
     @input = ""
     until @input == "exit"
-      
-    #elsif input == 'explore'
       get_countries
       country_list
       get_selection
-      #explore_more
     end 
     exit 
   end
@@ -28,7 +35,8 @@ class BabbelExplorer::CLI
   end
 
   def country_list #lists countries 
-    puts "\nChoose a country by its corresponding number\n".yellow
+    puts "\nChoose a country by its corresponding number or type 'q' to quit".yellow.bold
+    puts "\nScroll down to view country list\n".yellow
     puts "\n"
     @countries.each_with_index do |country, index|
       if index == 0 
@@ -37,24 +45,24 @@ class BabbelExplorer::CLI
       puts "#{index}. #{country.name}" .yellow
       end
     end
-    puts "\nScroll up to view countries\n".yellow.bold
+    puts "\nScroll up to view countries\n".yellow
+    
   end
   
   def get_selection
     chosen_country = gets.strip.to_i
     if valid_input(chosen_country, @countries)
       show_lang_blurb(chosen_country)
-    # elsif !valid_input(chosen_country, @countries)
-    #   invalid_input unless chosen_country == "exit"
     else
       explore_more
     end
   end
   
-  # def invalid_input 
-  #   puts "\nInvalid Command!".red.bold
-  #   puts "Please try again\n".red
-  # end
+  def invalid_input 
+    puts "\nInvalid Command!".red.bold
+    puts "Please try again\n".red
+    menu
+  end
 
   def valid_input(input, data)
     input.to_i <= data.length && input.to_i > 0
@@ -70,7 +78,7 @@ class BabbelExplorer::CLI
   end 
   
   def explore_more
-    puts "All done exploring for now? Hit any key to view country list again or type 'exit' to exit.".yellow
+    puts "All done exploring for now? Hit any key to view country list again or type 'exit' to exit.".red
     @input = gets.strip.downcase 
   end  
   
