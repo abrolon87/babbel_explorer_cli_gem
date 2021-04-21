3824b06f94a00d3b5bb53ff67423284d4ce5ba56
Amanda Rolon <abrolon87@gmail.com>
2021-04-20 21:11:25 -0400

    scrapes countries and languages from new source

--- lib/babbel_explorer/scraper.rb
+++ lib/babbel_explorer/scraper.rb
@@ -1,23 +1,14 @@
 class BabbelExplorer::Scraper
   
   def self.scrape_countries 
-    doc = Nokogiri::HTML(open("https://www.cia.gov/the-world-factbook/countries/") )
+    doc = Nokogiri::HTML(URI.open("https://www.factmonster.com/world/countries/languages-by-countries"))
     
-    #countries = doc.css("table#fieldListing tr")
-    countries = doc.css("div#index-content-section.container.pv120-90")
-    binding.pry
-    countries.each do |country|
-      name = country.css("div.col-lg-9.col-md12.col-sm-12").text.strip
-      binding.pry
-      puts name
-      # name = country.css("td.country").text.strip
-      # language = country.css("div.category_data.subfield.text").text
-      
+    doc.css('.sgmltable tr').each do |l|
+
+      name = l.children[0].text
+      language = l.children[1].text
+        
       BabbelExplorer::Country.new(name, language)
     end
   end      
 end
-
-
-
-
